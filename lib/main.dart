33ae99123e26bb571cb3d9bfc45b7b8a30a4188b
moriso <moriso.inc@gmail.com>
2023-03-29 17:58:11 -0300

    added a navigation rail to handle wide screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Remove the destination.dart import, it's not required
 import 'models/data.dart' as data;
 import 'models/models.dart';
+import 'widgets/disappearing_bottom_navigation_bar.dart'; // Add import
+import 'widgets/disappearing_navigation_rail.dart'; // Add import
 import 'widgets/email_list_view.dart';
 
 void main() {
@@ -37,21 +40,70 @@ class _FeedState extends State<Feed> {
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  int selectedIndex = 0;
+  // Add from here...
+  bool wideScreen = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+    wideScreen = width > 600;
+  }
+  // ... to here.
+
   @override
   Widget build(BuildContext context) {
+    // Modify from here...
     return Scaffold(
-      body: Container(
-        color: _backgroundColor,
-        child: EmailListView(
-          currentUser: widget.currentUser,
-        ),
+      body: Row(
+        children: [
+          if (wideScreen)
+            DisappearingNavigationRail(
+              selectedIndex: selectedIndex,
+              backgroundColor: _backgroundColor,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          Expanded(
+            child: Container(
+              color: _backgroundColor,
+              child: EmailListView(
+                selectedIndex: selectedIndex,
+                onSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+                currentUser: widget.currentUser,
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: _colorScheme.tertiaryContainer,
-        foregroundColor: _colorScheme.onTertiaryContainer,
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: wideScreen
+          ? null
+          : FloatingActionButton(
+              backgroundColor: _colorScheme.tertiaryContainer,
+              foregroundColor: _colorScheme.onTertiaryContainer,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+      bottomNavigationBar: wideScreen
+          ? null
+          : DisappearingBottomNavigationBar(
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
     );
+    // ... to here.
   }
 }
